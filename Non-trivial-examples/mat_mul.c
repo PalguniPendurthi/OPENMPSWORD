ea952e29744b7871371257b3c782cae18acb69b5
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-05-11 22:26:48 -0600

    Update mat_mul.c

--- Non-trivial-examples/mat_mul.c
+++ Non-trivial-examples/mat_mul.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <omp.h>
+#define  SIZE 8192
 
+int main(void) {
+    int i,j;
+    double dtime;
+    static long A[SIZE][SIZE], b[SIZE],V[SIZE];
+    for (i=0; i<SIZE; i++) {
+        for (j=0; j<SIZE; j++) {
+            A[i][j] = i+j;
+        }
+    }
+    for (i=0; i<SIZE; i++) b[i] = i;
+
+    dtime = -omp_get_wtime();
+    #pragma omp parallel for private(j) //comment out for one thread
+    for(i=0; i<SIZE; i++) {
+        long sum = 0;
+        for(j=0; j<SIZE; j++) {
+            sum += A[i][j]*b[j];
+        }
+        V[i] += sum;
+    }    
+    dtime += omp_get_wtime();
+    printf("The time taken for calculation is: %lf\n", dtime);
+
+    return 0;
+}
